f28007642ccd92a6bd3344cb38cbbad52c86a431
juanse07ljc <juanse07ljc@gmail.com>
2024-11-26 00:10:31 -0500

    responsive

--- proyecto-final-nov/js.js
+++ proyecto-final-nov/js.js
@@ -11,56 +11,30 @@ boton_inicio.addEventListener("mouseleave",()=>{
     boton_inicio.style.background='rgb(117, 117, 252)'
 
 })
-
-
-
-
-
-
-
-
-
     /* BOTON GITHUB */
     const github=document.getElementById('github')
 
     github.addEventListener("mouseenter",()=>{
         github.style.background='rgb(111, 176, 232';
     });
-
-
         github.addEventListener("mouseleave",()=>{
             github.style.background='rgb(117, 117, 252)';
     });
-
-
-
-
     /*BOTON REDES SOCIALES */
     const boton_redes_sociales=document.getElementById('boton_redes_sociales')
     boton_redes_sociales.addEventListener("mouseenter",()=>{
-        boton_redes_sociales.style.background='rgb(111, 176, 232';
-    
-    });
-
-
-
+        boton_redes_sociales.style.background='rgb(111, 176, 232'});
 
         boton_redes_sociales.addEventListener("mouseleave",()=>{
-    boton_redes_sociales.style.background='rgb(117, 117, 252)';
-
-    });
-
-
+    boton_redes_sociales.style.background='rgb(117, 117, 252)'});
+    
     /* BOTON MIS VIAJES */
     const boton_mis_viajes=document.getElementById('boton_mis_viajes')
     boton_mis_viajes.addEventListener("mouseenter",()=>{
         boton_mis_viajes.style.background='rgb(111, 176, 232';
     });
-
-
     boton_mis_viajes.addEventListener("mouseleave",()=>{
         boton_mis_viajes.style.background="rgb(117, 117, 252)";
-
     });
 
     
